122f42a6082af73ff218804eece80784895df878
tareque-bin-habib <shad.tbh@gmail.com>
2022-12-20 01:15:24 +0600

    responsive added

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -7,7 +7,7 @@ const Bio = () => {
     return (
         <div className='container p-5'>
             <div className='row row-cols-lg-2 row-cols-md-2 row-cols-1 d-flex align-items-center justify-content-center'>
-                <div>
+                <div className='bio-details'>
                     <h1 className='fw-bold'>Photography is not an <br />
                         Exact <span className='title-name'>Science</span> </h1>
                     <p>Making good on the Glasgow Climate Pact: a call to action to achieve one gigaton of emissions reductions from forests by 2025</p>
