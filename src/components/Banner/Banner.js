ec0cd9cafe08e63643e30c0d02401019ee1635bb
tareque-bin-habib <shad.tbh@gmail.com>
2022-12-20 12:36:02 +0600

    title changed

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,6 +4,7 @@ import image1 from '../../image/banner/13.jpg'
 import image2 from '../../image/banner/willbl_feat.jpg'
 import image3 from '../../image/banner/nintchdbpict000286875803.webp'
 import './Banner.css'
+import { Button } from 'react-bootstrap';
 
 const Banner = () => {
     return (
@@ -13,17 +14,21 @@ const Banner = () => {
                     <Carousel.Item>
                         <div className='banner-img-1 banner-size d-flex align-items-center justify-content-center'>
                             <h1 className='text-white'>Rise and fall of the <br /> dog-like hyenas</h1>
+
                         </div>
+
 
                     </Carousel.Item>
                     <Carousel.Item>
                         <div className='banner-img-2 banner-size d-flex align-items-center justify-content-center'>
                             <h1 className='text-white'>The Cheetah’s Wild Life is <br /> very popular to all</h1>
+
                         </div>
                     </Carousel.Item>
                     <Carousel.Item>
                         <div className='banner-img-3 banner-size d-flex align-items-center justify-content-center'>
                             <h1 className='text-white'>Family of Lion In the deep <br /> forest is amazing</h1>
+
                         </div>
                     </Carousel.Item>
                 </Carousel>
